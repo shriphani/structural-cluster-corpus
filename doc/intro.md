3397f35008ddcf3696b4af8feb513b8e785401de
Shriphani Palakodety <shriphanip@gmail.com>
2014-08-17 16:44:58 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to structural_cluster_corpus
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/structural_cluster_corpus/core_test.clj
@@ -0,0 +1,7 @@
+(ns structural-cluster-corpus.core-test
+  (:require [clojure.test :refer :all]
+            [structural-cluster-corpus.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
