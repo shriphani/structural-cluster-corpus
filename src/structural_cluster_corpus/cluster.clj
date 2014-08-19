0a9d185c21d59095766b451a163ac350e12b7643
Shriphani Palakodety <shriphanip@gmail.com>
2014-08-18 20:04:29 -0400

    clustering using single linkage and xpaths

--- src/structural_cluster_corpus/cluster.clj
+++ src/structural_cluster_corpus/cluster.clj
@@ -1,24 +1,115 @@
 (ns structural-cluster-corpus.cluster
   "Clustering algorithms")
 
+(defn stream-clustering-iterate
+  [clusters merge?]
+  (let [new-clusters
+        (reduce
+         (fn [clusters-so-far cluster]
+           (let [to-merge (first
+                           (filter
+                            (fn [cluster-seen]
+                              (merge? cluster-seen cluster))
+                            clusters-so-far))
+
+                 to-merge-index (if (nil? to-merge)
+                                  -1
+                                  (.indexOf clusters-so-far to-merge))]
+             (if (neg? to-merge-index)
+               (into [] (cons cluster clusters-so-far))
+               (assoc clusters-so-far
+                 to-merge-index
+                 (concat cluster
+                         (get clusters to-merge-index))))))
+         []
+         clusters)]
+    (println :iterating!)
+    (if (= (count new-clusters)
+           (count clusters))
+      new-clusters
+      (recur new-clusters merge?))))
+
 (defn stream-clustering
   [points belongs?]
-  (reduce
-   (fn [clusters pt]
-     (let [similar (first
-                    (filter
-                     (fn [c]
-                       (belongs? pt c))
-                     clusters))
+  (let [initialized
+        (reduce
+         (fn [clusters pt]
+           (let [sorted-clusters (reverse
+                                  (sort-by
+                                   #(-> % count)
+                                   clusters))
+                 
+                 similar (first
+                          (filter
+                           (fn [c]
+                             (belongs? pt c))
+                           sorted-clusters))
 
-           cluster-index (if (nil? similar)
-                           -1
-                           (.indexOf clusters similar))]
-       (if (neg? cluster-index)
-         (into [] (cons [pt] clusters))
-         (assoc clusters
-           cluster-index
-           (concat [pt]
-                   (get clusters cluster-index))))))
-   []
-   points))
+                 cluster-index (if (nil? similar)
+                                 -1
+                                 (.indexOf clusters similar))]
+             (if (neg? cluster-index)
+               (into [] (cons [pt] clusters))
+               (assoc clusters
+                 cluster-index
+                 (concat [pt]
+                         (get clusters cluster-index))))))
+         []
+         points)]
+    (stream-clustering-iterate initialized
+                               (fn [c1 c2]
+                                 (some
+                                  (fn [p]
+                                    (belongs? p c2))
+                                  c1)))))
+
+(defn stream-clustering-max-linkage
+  [points belongs?]
+  (let [initialized
+        (reduce
+         (fn [clusters pt]
+           (let [sorted-clusters (reverse
+                                  (sort-by
+                                   #(-> % count)
+                                   clusters))
+                 
+                 similar (first
+                          (filter
+                           (fn [c]
+                             (belongs? pt c))
+                           sorted-clusters))
+
+                 cluster-index (if (nil? similar)
+                                 -1
+                                 (.indexOf clusters similar))]
+             (if (neg? cluster-index)
+               (into [] (cons [pt] clusters))
+               (assoc clusters
+                 cluster-index
+                 (concat [pt]
+                         (get clusters cluster-index))))))
+         []
+         points)]
+    (stream-clustering-iterate initialized
+                               (fn [c1 c2]
+                                 (every?
+                                  (fn [p]
+                                    (belongs? p c2))
+                                  c1)))))
+
+(defn better-stream-clustering
+  [points similar?]
+  (let [initial-assignments (reduce
+                             (fn [[assignments next-cluster] pt]
+                               (println pt)
+                               (let [assign-attempt (first
+                                                     (filter
+                                                      (fn [[c-pt i]]
+                                                        (similar? c-pt pt))
+                                                      assignments))]
+                                 (if-not (nil? assign-attempt)
+                                   [(merge assignments {pt (second assign-attempt)}) next-cluster]
+                                   [(merge assignments {pt next-cluster}) (inc next-cluster)])))
+                             [{} 0]
+                             points)]
+    (first initial-assignments)))
--- src/structural_cluster_corpus/core.clj
+++ src/structural_cluster_corpus/core.clj
@@ -3,84 +3,89 @@
   (:require [clojure.java.io :as io]
             [structural-cluster-corpus.cluster :as cluster]
             [subotai.warc.warc :as warc]
-            [subotai.structural-similarity.core :as structural-similarity])
-  (:use [clojure.pprint :only [pprint]]))
+            [subotai.structural-similarity.core :as structural-similarity]
+            [subotai.structural-similarity.xpath-text :as xpath-text])
+  (:use [clojure.pprint :only [pprint]]
+        [subotai.structural-similarity.utils :only [cosine-similarity]]))
 
 (defn response-html-records
   [records]
   (filter
    (fn [r]
-     (and (= (-> r :warc-type) "response")))
+     (and (= (-> r :warc-type) "response")
+          (re-find #"response" (-> r :content-type))))
    records))
 
-(defn records->corpus
+(defn records->corpus-xpaths
   [records]
   (reduce
    (fn [acc r]
      (merge acc {(:warc-target-uri r)
-                 (:payload r)}))
+                 (try (->> r
+                           :payload
+                           xpath-text/page-text-xpaths
+                           xpath-text/char-frequency-representation)
+                      (catch Exception e nil))}))
    {}
    records))
 
-(defn handle-warc-file
+(defn cluster-single-linkage-xpaths
+  [data-records]
+  (let [corpus (records->corpus-xpaths data-records)
+
+        uris (map first corpus)
+        
+        similar? (fn [x y]
+                   ;; (println :x x :y y :sim (cosine-similarity (corpus x)
+                   ;;                                            (corpus y)))
+                   (>= (try (cosine-similarity (corpus x)
+                                               (corpus y))
+                            (catch Exception e 0))
+                       xpath-text/*sim-thresh*))
+
+        belongs? (fn [pt cluster]
+                   (some
+                    #(similar? % pt)
+                    cluster))]      
+    (cluster/stream-clustering uris
+                               belongs?)))
+
+(defn cluster-max-linkage-xpaths
+  [data-records]
+  (let [corpus (records->corpus-xpaths data-records)
+
+        uris (map first corpus)
+        
+        similar? (fn [x y]
+                   ;; (println :x x :y y :sim (cosine-similarity (corpus x)
+                   ;;                                            (corpus y)))
+                   (>= (try (cosine-similarity (corpus x)
+                                               (corpus y))
+                            (catch Exception e 0))
+                       xpath-text/*sim-thresh*))
+
+        belongs? (fn [pt cluster]
+                   (every?
+                    #(similar? % pt)
+                    cluster))]      
+    (cluster/stream-clustering-max-linkage uris
+                                           belongs?)))
+
+(defn handle-warc-file-100
   [a-warc-file]
   (let [warc-stream (warc/warc-input-stream a-warc-file)
         records (warc/stream-warc-records-seq warc-stream)
 
-        data-records (response-html-records records)
-
-        corpus (records->corpus data-records)
-
-        uris (map first corpus)
-
-        uri-belongs-1 (fn [pt cluster]
-                        (some
-                         (fn [c-pt]
-                           (let [c-body (corpus c-pt)
-                                 pt-body (corpus pt)]
-                             (structural-similarity/similar? c-body
-                                                             pt-body)))
-                         cluster))
-
-        uri-belongs-2 (fn [pt cluster]
-                        (every?
-                         (fn [c-pt]
-                           (let [c-body (corpus c-pt)
-                                 pt-body (corpus pt)]
-                             (structural-similarity/similar? c-body
-                                                             pt-body)))
-                         cluster))
-
-        uri-belongs-3 (fn [pt cluster]
-                        (let [matched (count
-                                       (filter
-                                        (fn [c-pt]
-                                          (let [c-body (corpus c-pt)
-                                                pt-body (corpus pt)]
-                                            (structural-similarity/similar? c-body
-                                                                            pt-body)))
-                                        cluster))]
-                          (>= matched (/ (count cluster)
-                                         2))))]
-    (do
-      (with-open [wrtr-1 (io/writer (str a-warc-file
-                                         ".clusters-single"))
-                  wrtr-2 (io/writer (str a-warc-file
-                                         ".clusters-all"))
-                  wrtr-3 (io/writer (str a-warc-file
-                                         ".clusters-majority"))]
-        (pprint (cluster/stream-clustering uris
-                                           uri-belongs-1)
-                wrtr-1)
-        (pprint (cluster/stream-clustering uris
-                                           uri-belongs-2)
-                wrtr-2)
-        (pprint (cluster/stream-clustering uris
-                                           uri-belongs-3)
-                wrtr-3))
-      (.close warc-stream))))
+        data-records (take 1000 (response-html-records records))]
+    (reverse
+     (sort-by
+      count
+      (cluster-max-linkage-xpaths data-records)))))
 
 (defn -main
   [& args]
-  (handle-warc-file
-   (first args)))
+  (with-open [wrtr (io/writer (second args))]
+    (pprint
+     (handle-warc-file-100
+      (first args))
+     wrtr)))
